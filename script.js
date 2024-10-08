22117ff12b1f4425cc1c7e0f21e05e1551765f28
praneelbhag <praneelb64@gmail.com>
2024-10-08 16:32:48 -0500

    js update

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
+// Smooth scrolling for navigation links
+document.querySelectorAll('nav ul li a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+// Dynamic typing effect
+const typingText = "High School Student | Researcher | STEM Enthusiast";
+let index = 0;
+
+function type() {
+    if (index < typingText.length) {
+        document.getElementById("typing").innerHTML += typingText.charAt(index);
+        index++;
+        setTimeout(type, 100);
+    }
+}
+
 window.onload = type;
 
 // Back to Top Button
